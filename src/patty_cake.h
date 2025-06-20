fc2694ee054802a7e4252c65588f9e7f45a77f68
john <jhoney510@gmail.com>
2025-06-20 23:15:09 +0900

    .

--- src/patty_cake.h
+++ src/patty_cake.h
@@ -2,8 +2,10 @@
 #define PATTY_CAKE_PATTY_CAKE_H_
 
 
-#include "./cakes/base.h"
 #include "./cakes/_piece.h"
+#include "./cakes/base.h"
+#include "./cakes/web_rtc_cake.h"
+#include "./cakes/web_socket_cake.h"
 
 
 #endif  // ifndef PATTY_CAKE_PATTY_CAKE_H_
